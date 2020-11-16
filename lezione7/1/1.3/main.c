8511f15251a6e28ce566622c53ea298ac8b44f87
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2020-11-16 16:48:48 +0100

    Complete exercises on Corrections

---
+++ lezione7/1/1.3/main.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "def.h"
+#include "A.h"
+
+/*extern SD fA(SD*);*/
+int main(void){ 
+	SD A={33.666}, B, C={-15.21}; 
+
+	B = fA( &A );
+	printf("%f\n", B.x );
+        if( B.x > 1.1 )
+                return( 1 );
+        else
+                return( 0 );
+}
+
+
