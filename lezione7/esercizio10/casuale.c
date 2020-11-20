fbb8a0298a42ead9a6e9e0a835e952be5e7e9b92
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2020-11-20 22:31:59 +0100

    Correct ex 10

--- lezione7/esercizio10/casuale.c
+++ lezione7/esercizio10/casuale.c
@@ -5,7 +5,7 @@
 int main() {
 	int count = 0;
 	srand(time(NULL));
-	while(++count && (rand() % 3 == 0));
+	while(++count && (rand() % 10 == 3));
 	printf("Numero di chiamate random: %d\n", count);
 	return 0;
 }
