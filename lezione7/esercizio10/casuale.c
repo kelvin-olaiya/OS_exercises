d72376845082aa6b33682e1b1ac4feef6bde4a05
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2020-11-20 22:27:46 +0100

    Complete ex 10, lezione7

---
+++ lezione7/esercizio10/casuale.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+	int count = 0;
+	srand(time(NULL));
+	while(++count && (rand() % 3 == 0));
+	printf("Numero di chiamate random: %d\n", count);
+	return 0;
+}
